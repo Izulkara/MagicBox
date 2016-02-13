806dd2e993fe7421582ea3e8d78608e11f7c2849
Izulkara <izulkara@gmail.com>
2016-02-13 11:00:46 -0800

    Battlefield

---
+++ FreeMagic01/Assets/Tile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public Tile NorthTile;
+    public Tile WestTile;
+    public Tile SouthTile;
+    public Tile EastTile;
+    public GameObject Occupier;
+
+    // Use this for initialization
+    void Start () {
+        //NorthTile = Instantiate(Tile);
+        //NorthTile = new Tile();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
