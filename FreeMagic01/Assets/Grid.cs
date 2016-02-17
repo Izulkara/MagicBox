1d729568688e2be620e0ca35a5a65a6f65d937b2
rmeier23 <rmeier@uw.edu>
2016-02-17 04:11:53 -0800

    Added 2D array object for titles on the grid

--- FreeMagic01/Assets/Grid.cs
+++ FreeMagic01/Assets/Grid.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Grid : MonoBehaviour {
+
+public class Grid : MonoBehaviour
+{
+    private Tile[,] tiles2D;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        tiles2D = new Tile[10, 10];
+        Tile[] tiles = gameObject.GetComponentsInChildren<Tile>();
+
+        foreach (Tile t in tiles)
+        {
+            tiles2D[(int)t.transform.localPosition.x,(int)t.transform.localPosition.z] = t;
+            
+        }
+
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        
+    }
 }
