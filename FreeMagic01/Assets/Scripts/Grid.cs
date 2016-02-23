fc3db86f185cbf92af4783f08ab7c2c8f86cc3d7
Andrew Gates <ag8833@aol.com>
2016-02-23 13:44:13 -0800

    Added in-game menu

--- FreeMagic01/Assets/Scripts/Grid.cs
+++ FreeMagic01/Assets/Scripts/Grid.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
+    bool Paused = false;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == false)
+            {
+                Paused = true;
+                Application.LoadLevel(1);
+            }
+            else
+            {
+                Paused = false;
+            }
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == false)
+            {
+                Paused = true;
+                Application.LoadLevel(1);
+            }
+            else
+            {
+                Paused = false;
+            }
+        }
+    }
 }
---
+++ FreeMagic01/Assets/Scripts/pauseMenuScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class pauseMenuScript : MonoBehaviour
+{
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+    bool Paused = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+
+    }
+
+    public void ExitPress() 
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+
+    }
+
+    public void NoPress()
+    { 
+        quitMenu.enabled = false; 
+        startText.enabled = true; 
+        exitText.enabled = true;
+
+    }
+
+    public void StartLevel() 
+    {
+        Application.LoadLevel(2); 
+    }
+
+    public void ExitGame() 
+    {
+        Application.LoadLevel(0);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if(Paused == false)
+            {
+                Paused = true;
+                Start();
+            }
+            else
+            {
+                Paused = false;
+                StartLevel();
+            }
+        }
+	}
+}
