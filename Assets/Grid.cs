fe6feb7a2756da884dd801772d53c70fe929582b
unknown <wthomase@myuw.edu>
2016-02-23 14:20:07 -0800

    updated

---
+++ Assets/Grid.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Grid : MonoBehaviour
+{
+    private Tile[,] tiles2D;
+	private Tile currentlySelected;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tiles2D = new Tile[10, 10];
+        Tile[] tiles = gameObject.GetComponentsInChildren<Tile>();
+
+        foreach (Tile t in tiles)
+        {
+            tiles2D[(int)t.transform.localPosition.x,(int)t.transform.localPosition.z] = t;
+            
+        }
+
+        
+	}
+
+	public void attemptMove() {
+
+	}
+
+	public bool tileSelected() {
+		return currentlySelected != null;
+	}
+
+	// Changes the currently selected tile on the grid 
+	public void changeSelected(Tile newSelected) {
+		// if we have a tile selected, unhighlight the tiles highlighted for movement
+		// and unhighlight the currently selected tile
+		if (currentlySelected != null) {
+			currentlySelected.overrideHighlight ();
+			toggleHighlightForMovement ();
+		}
+
+		// set the new tile to be the currently selected
+		// toggle the highlighting for its movement 
+		currentlySelected = newSelected;
+		toggleHighlightForMovement ();
+	}
+
+	private void toggleHighlightForMovement() {
+		if (currentlySelected.NorthTile != null) {
+			currentlySelected.NorthTile.overrideHighlight ();
+		} 
+
+		if (currentlySelected.SouthTile != null) {
+			currentlySelected.SouthTile.overrideHighlight ();
+		}
+
+		if (currentlySelected.EastTile != null) {
+			currentlySelected.EastTile.overrideHighlight ();
+		}
+
+		if (currentlySelected.WestTile != null) {
+			currentlySelected.WestTile.overrideHighlight ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public Tile NorthTile;
+    public Tile WestTile;
+    public Tile SouthTile;
+    public Tile EastTile;
+    public GameObject Occupier;
+	public GameObject Highlight;
+
+    // Use this for initialization
+    void Start () {
+
+        //NorthTile = Instantiate(Tile);
+        //NorthTile = new Tile();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// External method used to toggle the highlight graphic without a mouse event
+	public void overrideHighlight() {
+		Highlight.SetActive (!Highlight.activeSelf);
+	}
+
+	void OnMouseDown() {
+		// Toggle the highlight graphic on the tile
+		Highlight.SetActive (!Highlight.activeSelf);
+
+		// Tell the grid that this tile has been selected
+		// If the grid has a tile selected tell the grid to attempt to move to this position
+		GameObject grid = GameObject.Find ("Grid");
+		Grid gridScript = grid.GetComponent<Grid> ();
+		gridScript.changeSelected(this);
+
+	}
+
+
+		
+}
