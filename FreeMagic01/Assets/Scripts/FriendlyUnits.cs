cd3c8e0092bc75596b75c9018701e41c1364cdee
rmeier23 <rmeier@uw.edu>
2016-02-28 03:25:32 -0800

    Added files via upload
    Includes code for unit selection

---
+++ FreeMagic01/Assets/Scripts/FriendlyUnits.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyUnits : MonoBehaviour {
+    private Unit[] myUnits;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Unit[] getMyUnits()
+    {
+
+        return myUnits;
+    }
+}
