3a55ad352e795130404e8149c6cbc1f86f847f70
Ag8833 <ag8833@aol.com>
2016-02-29 01:41:03 -0800

    Combined with Leslie's updates

---
+++ FreeMagic01/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float max_health = 200f;
+	public float curr_health = 0f;
+
+	// Use this for initialization
+	void Start () {
+		curr_health = max_health;
+		InvokeRepeating ("DecHealth", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void DecHealth() {
+		curr_health -= 20f;
+	}
+} 
